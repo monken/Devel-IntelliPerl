37027651677888fd2331542cb0a8ce4bc550a22b
Moritz Onken <onken@houseofdesign.de>
2009-06-10 18:28:22 +0200

    initial commit

---
+++ lib/Devel/IntelliPerl.pm
@@ -0,0 +1,107 @@
+package Devel::IntelliPerl;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Devel::IntelliPerl - The great new Devel::IntelliPerl!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Devel::IntelliPerl;
+
+    my $foo = Devel::IntelliPerl->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Moritz Onken, C<< <onken at netcubed.de> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-devel-intelliperl at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Devel-IntelliPerl>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Devel::IntelliPerl
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Devel-IntelliPerl>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Devel-IntelliPerl>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Devel-IntelliPerl>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Devel-IntelliPerl/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Moritz Onken, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Devel::IntelliPerl
